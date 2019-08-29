e6ded2e85eaf3b0c019f730346250ef40f1aea64
Notorious <notoriousbpp@gmail.com>
2019-08-28 22:40:35 -0700

    removed redundant breakpoints file

--- src/components/FormUlaRow/FormUlaRow.tsx
+++ src/components/FormUlaRow/FormUlaRow.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {FC, ReactNode} from "react";
-import { breakpoints } from '../../utils'
+import { breakpoints } from '../../commons'
 import styled from 'styled-components'
 
 type columns = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
--- src/utils/breakpoints.ts
+++
@@ -1,7 +0,0 @@
-export const breakpoints = {
-  xs: '@media (min-width: 0)',
-  sm: '@media (min-width: 648px)',
-  md: '@media (min-width: 768px)',
-  lg: '@media (min-width: 1024px)',
-  xl: '@media (min-width: 1920px)',
-};
